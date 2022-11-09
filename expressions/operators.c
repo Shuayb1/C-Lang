8438a1ebb53d033205b5347cb3f1f9dfb7250e15
Shuayb Popoola <shuaybpopoola@gmail.com>
2022-11-09 23:44:27 +0100

    arithmetic operator

---
+++ expressions/operators.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main(void)
+{
+	int a = 6, b = 44, c = 4, d;
+
+	printf("value of a + b/c = %i\n", d = a + b/c);
+
+	printf("value of (a + b)/c = %i\n", d = (a + b)/c);
+
+	printf("value of a + b/c = %i\n", d = a + b * c);
+
+	return (0);
+
+
+}
